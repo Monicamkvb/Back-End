e7373994cccd9d92d5db050d6f80d1f8a580fb8d
Monicamkvb <monicakarolbraga@gmail.com>
2025-03-18 17:44:58 -0300

    carrinho

--- carrinho/index.php
+++ carrinho/index.php
@@ -9,10 +9,11 @@ class Produto{
     public $valor;
     public $desc;
 
-    public function __construct($nome,$valor,$desc){
-        $this->$nome = $nome;
-        $this->$valor = $valor;
-        $this->$desc = $desc;
+    public function __construct($id,$nome,$valor,$desc){
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->valor = $valor;
+        $this->desc = $desc;
     }
 }
 
@@ -22,17 +23,29 @@ class Produto{
 //verifica se algo está null
 if (!isset($_SESSION['carrinho'])){
     $_SESSION['carrinho'] = [];
-
+}
 //adiciona um produto
 if (isset($_GET['produto'])){
-    $produtosDados = new Produto($_GET['produto'],$_GET['valor'],$_GET['desc']);
+    $produtosDados = new Produto($_GET['id'],$_GET['produto'],$_GET['valor'],$_GET['desc']);
     $_SESSION['carrinho'][] = $produtosDados;
 }
-}
 
-if (isset($_GET['limpar'])) {
+//limpar carrinho
+if (isset($_GET['limparCarrinho'])){
     $_SESSION['carrinho'] = [];
 }
+
+//remover um item
+if(isset($_GET['removerItem'])){
+    foreach($_SESSION['carrinho'] as $id => $produto){
+    if($produto->id == $_GET['removerItem']){
+        unset($_SESSION['carrinho'][$id]);
+        $_SESSION['carrinho'] = array_values($_SESSION['carrinho']);
+        }
+    }
+}
+
+// var_dump($_SESSION['carrinho']);
 
 ?>
 
@@ -47,21 +60,41 @@ if (isset($_GET['limpar'])) {
 <body>
     <h1>Carrinho de Compras</h1>
 
-    <p><a href="?produto=oleo&valor=39.99&desc='O melhor óleo capilar que'">Adicionar oleo</a></p>
-    <p><a href="?produto=Shampoo">Adicionar Shampoo</a></p>
-    <p><a href="?produto=MyWay">Adicionar MyWay</a></p>
-    <p><a href="?produto=Máscara de hidratação">Adicionar Máscara de hidratação</a></p>
+    <p><a href="?id=1&produto='Óleo'&valor=39.99&desc='O óleo que vai transformar a sua pele'">Adicionar oleo</a></p>
+    <p><a href="?id=2&produto=Shampoo&valor=60.00&desc='Shampoo Eudora, a salvação do seu cabelo'">Adicionar Shampoo</a></p>
+    <p><a href="?id=3&produto=MyWay&valor=300.00&desc='O perfume mais sofisticado do mercado'">Adicionar MyWay</a></p>
+    <p><a href="?id=4&produto=Máscaradehidratacao&valor=240.00&desc='Seu cabelo precisa de um milagre? Use nossa máscara'">Adicionar Máscara de hidratação</a></p>
+    <p><a href="?limparCarrinho=True">Limpar carrinho</a></p>
+
+    <?php
+    $valorTotal = 0;
+    foreach($_SESSION['carrinho'] as $produto){
+        $valorTotal += $produto->valor;
+    }
+
+    if($valorTotal == 0){
+        echo "O carrinho está vazio";
+    }else{
+        echo "O valor total R$ $valorTotal";
+    }
+    
+    ?>
 
     <h2>Produtos no Carrinho:</h2>
-    <?php if (count($_SESSION['carrinho']) > 0): ?>
-        <ul>
-            <?php foreach ($_SESSION['carrinho'] as $item): ?>
-                <li><?php echo $item; ?></li>
-            <?php endforeach; ?>
-        </ul>
-        <p><a href="?limpar=true">Limpar carrinho</a></p>
-    <?php else: ?>
-        <p>Seu carrinho está vazio.</p>
-    <?php endif; ?>
+
+    <ol>
+        <?php
+            // foreach(array as alias){}
+            // foreach(filmes as filme){ film.nome , filme.desc}
+            foreach($_SESSION['carrinho'] as $produto){
+               echo "<li> 
+                   <h1> $produto->nome </h1>
+                   <p> R$ $produto->valor </p>
+                   <p> $produto->desc </p>
+                   <a href='?removerItem=$produto->id'> x </a>
+                    </li>";
+            }
+      ?>
+    </ol>
 </body>
 </html>
